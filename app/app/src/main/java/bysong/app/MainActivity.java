4d891879bc1b590477dae07490a56719425bb518
DeveloperMobile <tico_todoseu_20@hotmail.com>
2016-08-08 23:00:56 -0300

    teste

--- app/app/src/main/java/bysong/app/MainActivity.java
+++ app/app/src/main/java/bysong/app/MainActivity.java
@@ -7,8 +7,12 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        lucas
+        // lucas
+        // tiago
+
     }
+
 }
